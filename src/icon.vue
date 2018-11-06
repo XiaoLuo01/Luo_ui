d453ae456595d85883d603c756df626eb8226630
Carol <fuxiaoluo304@163.com>
2018-11-07 00:23:10 +0800

    将 svg 代码整理到 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,26 @@
+<template>
+ <svg v-if="name" class="g-icon" aria-hidden="true">
+        <use :xlink:href="`#i-${name}`"></use>
+      </svg>
+</template>
+
+<script>
+export default {
+ data() {
+  return {
+
+  }
+ },
+ props: ['name'],
+ components: {
+
+ }
+}
+</script>
+
+<style scoped lang="scss">
+ .g-icon {
+            width: 1em;
+            height: 1em;
+        }
+</style>
