e0570d1323e0db1dcec11ad108cc2b118ff29efa
Carol <fuxiaoluo304@163.com>
2018-11-12 23:47:18 +0800

    添加 input 的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,43 +1,41 @@
 <template>
- <div class="wrapper" :class="{error}">
-     <input :value="value" :disabled="disabled" :readonly="readonly" type="text">
-     <template v-if="error">
-         <Icon name="error" class="icon-error"></Icon>
-         <span class="error-msg">{{error}}</span>
-     </template>
- </div>
+    <div class="wrapper" :class="{error}">
+        <input :value="value" :disabled="disabled" :readonly="readonly" type="text" @change="$emit('change', $event)" @input="$emit('input', $event)" @focus="$emit('focus', $event)" @blur="$emit('blur', $event)">
+        <template v-if="error">
+            <Icon name="error" class="icon-error"></Icon>
+            <span class="error-msg">{{error}}</span>
+        </template>
+    </div>
 </template>
 
 <script>
-import Icon from './icon'
+import Icon from "./icon"
 export default {
-    name: "VioInput",
- data() {
-  return {
-
+  name: "VioInput",
+  data() {
+    return {}
+  },
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String,
+      default: ""
+    }
+  },
+  components: {
+    Icon
   }
- },
- props: {
-     value: {
-         type: String,
-         default: ''
-     },
-     disabled: {
-         type:Boolean,
-         default: false
-     },
-     readonly: {
-         type: Boolean,
-         default: false
-     },
-     error: {
-         type: String,
-         default: ''
-     }
- },
- components: {
-     Icon
- }
 }
 </script>
 
@@ -48,42 +46,43 @@ $border-color-hover: #666;
 $border-raduis: 4px;
 $font-size: 12px;
 $borx-shadow-color: rgba(0, 0, 0, 0.5);
-$red: #F1453d;
+$red: #f1453d;
 .wrapper {
-    font-size: $font-size;
-    display: inline-flex;
-    align-items: center;
-    > :not(:last-child) {
-        margin-right: .5em;
-    }
-    &.error {
-        > input {
-            border-color: $red;
-        }
-    }
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  > :not(:last-child) {
+    margin-right: 0.5em;
+  }
+  &.error {
     > input {
-        height: $height;
-        border: 1px solid $border-color;
-        border-radius: $border-raduis;
-        padding: 0 8px;
-        font-size: inherit;
-        &:hover {
-            border-color: $border-color-hover;
-        }
-        &:focus {
-            box-shadow: inset 0 1px 3px $borx-shadow-color;
-        }
-        &[disabled], &[readonly] {
-            border-color: #aaa;
-            color: #aaa;
-            cursor: not-allowed;
-        }
+      border-color: $red;
     }
-    .icon-error {
-        fill: $red;
+  }
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-raduis;
+    padding: 0 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
     }
-    .error-msg {
-        color: $red;
+    &:focus {
+      box-shadow: inset 0 1px 3px $borx-shadow-color;
     }
+    &[disabled],
+    &[readonly] {
+      border-color: #aaa;
+      color: #aaa;
+      cursor: not-allowed;
+    }
+  }
+  .icon-error {
+    fill: $red;
+  }
+  .error-msg {
+    color: $red;
+  }
 }
 </style>
