398b0d358831fe9373536436bd150b3c620e95a0
Carol <fuxiaoluo304@163.com>
2018-11-15 00:00:50 +0800

    row 和 col 的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VioCol',
+    props: {
+        span: {
+            type: [Number, String]
+        }
+    },
+    data() {
+    return {
+
+    }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.col {
+  width: 50%;
+  height: 100px;
+  background: rgba(0, 153, 229, 0.7);
+  border: 1px solid blueviolet;
+
+  $class: col-;
+
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="row">
+    <slot></slot>
+ </div>
+</template>
+
+<script>
+export default {
+ data() {
+  return {
+
+  }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped lang="scss">
+.row {
+    display: flex;
+}
+</style>
