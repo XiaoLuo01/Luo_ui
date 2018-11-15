269f56183cedc821e011d383421f2eeaa6214b37
Carol <fuxiaoluo304@163.com>
2018-11-15 23:47:16 +0800

    重构 row 和 col 的代码

--- src/col.vue
+++ src/col.vue
@@ -1,8 +1,6 @@
 <template>
-    <div class="col" :class="[`span && col-${span}`, `offset && offset-${offset}`]" :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid blueviolet;height: 50px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
@@ -19,7 +17,19 @@ export default {
   },
   data() {
     return {
-        gutter: 0
+      gutter: 0
+    }
+  },
+  computed: {
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px"
+      }
+    },
+    colClass() {
+      let { span, offset } = this
+      return [span && `col-${span}`, offset && `offset-${offset}`]
     }
   },
   components: {}
@@ -30,8 +40,8 @@ export default {
 .col {
   width: 50%;
   height: 50px;
-//   background: rgba(0, 153, 229, 0.7);
-//   border: 1px solid blueviolet;
+  //   background: rgba(0, 153, 229, 0.7);
+  //   border: 1px solid blueviolet;
 
   $class: col-;
 
--- src/row.vue
+++ src/row.vue
@@ -1,35 +1,39 @@
 <template>
- <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2  + 'px'}">
-    <slot></slot>
- </div>
+    <div class="row" :style="rowStyle">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'VioRow',
-    props: {
-        gutter: {
-            type: [Number, String]
-        }
-    },
- data() {
-  return {
-
-  }
- },
- components: {
-
- },
- mounted() {
+  name: "VioRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    rowStyle() {
+      return {
+        marginLeft: -this.gutter / 2 + "px",
+        marginRight: -this.gutter / 2 + "px"
+      }
+    }
+  },
+  components: {},
+  mounted() {
     this.$children.forEach(vm => {
-        vm.gutter = this.gutter
+      vm.gutter = this.gutter
     })
- }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .row {
-    display: flex;
+  display: flex;
 }
 </style>
