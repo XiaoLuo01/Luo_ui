03cd211d2dea72a1b90ef81030eb02afc3ec56c2
Carol <fuxiaoluo304@163.com>
2018-11-18 23:07:53 +0800

    添加 plugin 和 toast

---
+++ src/toast.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="toast">
+    </div>
+</template>
+
+<script>
+export default {
+  name: "VioToast",
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
