19763be45e3719a5638855e0b87105123928db85
Carol <fuxiaoluo304@163.com>
2018-11-25 11:55:53 +0800

    增大 tabs-item 的可点击区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -52,6 +52,10 @@ export default {
   // flex-grow: 1;
   flex-shrink: 0;
   padding: 0 1em;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center;
   &.active {
     background: red;
   }
