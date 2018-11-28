0b01dc1f3768eaaf29c9f0bff3abd0f506b25a87
Carol <fuxiaoluo304@163.com>
2018-11-28 22:36:45 +0800

    解决容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
  <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
         <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+        <slot></slot>
+    </span>
  </div>
 </template>
 
@@ -24,6 +26,11 @@ export default {
          if(this.visible) {
              // 监听外面的点击事件
              this.$nextTick(() => {
+                 document.body.appendChild(this.$refs.contentWrapper)
+                 let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                 console.log(left, top)
+                 this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                 this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                  let eventHandler = () => {
                      this.visible = false
                      // 监听完毕之后就要移除, 不然的话每一次点击都会新增一个监听器
@@ -33,6 +40,9 @@ export default {
              })
          }
      }
+ },
+ mounted() {
+     
  }
 }
 </script>
@@ -42,12 +52,12 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5)
-    }
+}
+.content-wrapper {
+    width: 200px;
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
 }
 </style>
