8eb83a620b4d69f28402066a374731f3c29c7a4c
Carol <fuxiaoluo304@163.com>
2018-12-01 10:14:10 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
         <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block">
         <slot></slot>
     </span>
  </div>
@@ -30,6 +30,9 @@ export default {
      onClickDocument(e) {
         // TODO: 此处还有 bug , refs.popover 里面获取不到 contentWrapper
         if(this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+            return
+        }
+        if(this.$refs.contentWrapper && (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))) {
             return
         }
         this.close()
@@ -64,16 +67,42 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$border-color: #333;
+$border-radius: 4px;
 .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
 }
 .content-wrapper {
-    width: 200px;
+    max-width: 20em;
+    background: #fff;
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    z-index: 999;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    filter: drop-shadow(0 0 3px rgba(0, 0, 0, 0.5));
     transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    word-break: break-all;
+    &::before, &::after {
+        content: '';
+        display: block;
+        border: 10px solid transparent;
+        width: 0;
+        height: 0px;
+        position: absolute;
+        left: 10px;
+    }
+    &::before {
+        border-top-color: black;
+        top: 100%;
+    }
+    &::after {
+        border-top-color: white;
+        top: calc(100% - 1px);
+    }
 }
 </style>
